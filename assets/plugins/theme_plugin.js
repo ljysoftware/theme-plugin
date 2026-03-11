c4b13c88df6db0dde2d377c58e4114e8882334da
ChrisChae00 <chrischae00@gmail.com>
2026-03-11 16:29:36 -0400

    feat: enhance theme plugin with theme selection modal
    - Implement openThemePicker action to display theme options (Light, Dark)
    - Add setTheme action to update the selected theme using simpleChat.api.updateTheme()

--- assets/plugins/theme_plugin.js
+++ assets/plugins/theme_plugin.js
@@ -1,7 +1,19 @@
-// Theme Plugin - placeholder
-// TODO: Implement theme switching (Light, Dark, Pink, Green)
 function onLoad() {
-  registerPluginActions({});
+  registerPluginActions({
+    openThemePicker: function () {
+      simpleChat.api.showModal({
+        title: 'Select Theme',
+        options: [
+          { label: 'Light', value: 'light' },
+          { label: 'Dark', value: 'dark' },
+        ],
+        callback: 'setTheme',
+      });
+    },
+    setTheme: function (theme) {
+      simpleChat.api.updateTheme(theme);
+    },
+  });
 
   return {
     name: 'Theme Plugin',
